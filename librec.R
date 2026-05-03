3029cbcb25aa71a12b3678091548341a910b52c9
Pedro Afonso Fernandes <pedro@KT587AA-AB9-SR5522PT-c6e2e475.lan>
2026-05-03 16:57:13 +0100

    Data change

--- librec.R
+++ librec.R
@@ -1,5 +1,5 @@
 #
-# librec.R - A small library of recursive methods in R, v01.22 (2026-02-25)
+# librec.R - A small library of recursive methods in R, v01.22 (2026-04-03)
 #
 # Adaptation to R with extensions of the Thomas Sargent's MatLab toolkits available at:
 #
